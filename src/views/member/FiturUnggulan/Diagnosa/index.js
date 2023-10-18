3d90592900a370757ccd36c2cf9d3e56730cdba6
anti009x <riskisuleman76@gmail.com>
2023-10-18 10:19:48 +0700

    Penambahan Front End Untuk Kamera Pada Fitur Diagnosa

--- src/views/member/FiturUnggulan/Diagnosa/index.js
+++ src/views/member/FiturUnggulan/Diagnosa/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert } from 'react-native';
-import messaging from '@react-native-firebase/messaging';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, PermissionsAndroid, Image } from 'react-native';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import StatusBarComponent from '../../../../components/StatusBar/StatusBarComponent';
 import { useNavigation } from '@react-navigation/native';
 import Heading from '../../../../components/Heading';
@@ -8,61 +8,53 @@ import Navigasi from '../../../../partials/navigasi';
 
 const Diagnosa = () => {
   const navigation = useNavigation();
-  const [fcmToken, setFcmToken] = useState(null);
+  const [cameraData, setCameraData] = useState(null);
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: "App Camera Permission",
+          message: "App needs access to your camera",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Camera permission given");
+        openCamera();  // Open the camera after permission is granted
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const openCamera = () => {
+    const options = {
+      mediaType: 'photo',
+      quality: 1,
+    };
+
+    launchCamera(options, (response) => {
+      if (response.didCancel) {
+        console.log('Cancelled');
+      } else if (response.errorCode) {
+        console.log(response.errorMessage);
+      } else {
+        const data = response.assets;
+        console.log(data);
+        setCameraData(data);  // Set camera data to state
+      }
+    });
+  };
 
   useEffect(() => {
-    const requestUserPermission = async () => {
-      try {
-        const authStatus = await messaging().requestPermission();
-        const enabled =
-          authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-          authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-        if (enabled) {
-          console.log('Authorization status:', authStatus);
-          await getFCMToken();
-        }
-      } catch (error) {
-        console.log('Error requesting permission:', error);
-      }
-    };
-
-    const getFCMToken = async () => {
-      try {
-        const newFcmToken = await messaging().getToken();
-        if (newFcmToken) {
-          setFcmToken(newFcmToken);
-          console.log('FCM Token:', newFcmToken);
-        }
-      } catch (error) {
-        console.log('Error getting FCM token:', error);
-      }
-    };
-
-    const unsubscribeOnMessage = messaging().onMessage(async remoteMessage => {
-      Alert.alert('New FCM Message', JSON.stringify(remoteMessage));
-    });
-
-    requestUserPermission();
-    return unsubscribeOnMessage;
+    requestCameraPermission();  // Request camera permission when the component mounts
   }, []);
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    content: {
-      flex: 1,
-      padding: 16,
-      flexDirection: 'column',
-    },
-    text: {
-      color: 'blue',
-      fontSize: 15,
-      fontFamily: 'verdana',
-      marginTop: 10,
-    },
-  });
 
   return (
     <View style={{ flex: 1 }}>
@@ -71,12 +63,46 @@ const Diagnosa = () => {
       <View style={styles.navigationHeader}>
         <StatusBarComponent />
       </View>
-      <View style={styles.content}>
-        <Text style={styles.text}>Disini Taruh FCM Token:</Text>
-
+      <View style={styles.card}>
+        <TouchableOpacity onPress={openCamera}>
+          <Text>
+            Tempat untuk membuka kamera
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.card1}>
+        <Text style = {{color:'white',  padding: 20,borderRadius: 10,elevation: 2,}}>
+          Tempat untuk hasil
+        </Text>
+        {cameraData && (
+          <Image
+            source={{ uri: cameraData[0].uri }}  // Display the image from camera
+            style={{ width: 350, height: 300 , marginTop:20, }}
+          />
+        )}
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 3,
+    alignItems: 'center',
+  },
+  card1: {
+    backgroundColor: '#BDC3C7',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 3,
+    alignItems: 'center',
+    marginTop:10,
+    flex :1,
+    
+  },
+});
 
 export default Diagnosa;
